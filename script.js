26b91e388540ac8e77f661f2ddfb86dcf084402f
Steven Thalblum <thalswim@yahoo.com>
2021-07-15 20:49:07 -0400

    Added function to plus operator, clear and AC buttons

--- script.js
+++ script.js
@@ -1,20 +1,64 @@
 const calcArea = document.querySelector('#calc-area');
 const buttons = document.querySelectorAll('.button');
+const clear = document.querySelector('#clear');
+const plus = document.querySelector('#plus');
+const allClear = document.querySelector('.clear-all');
+
+let numberOne = 0;
+let numberTwo = 0;
+let operation = "";
+let emptyArea = "F";
 
 function addNumber (num) {
-    calcArea.innerHTML += num;
+    calcArea.textContent += num;
 }
+function replaceNumber (num) {
+    calcArea.textContent = num;
+}
+function clearAll () {
+    numberOne = 0;
+    numberTwo = 0;
+    operation = "";
+    emptyArea = "F";
+    calcArea.textContent = "";
+}
+
+allClear.addEventListener('click', () => {
+    clearAll();
+})
 
 buttons.forEach(buttons => {
     buttons.addEventListener('click', () => {
-        addNumber(buttons.getAttribute('id'));
+        if (emptyArea == "F") {
+            addNumber(buttons.getAttribute('id'));
+        }
+        else {
+            replaceNumber(buttons.getAttribute('id'));
+            emptyArea = "F";
+        }
     })
 })
 
-function getNumber (id) {
+clear.addEventListener('click', () => {
+    calcArea.textContent = "";
+});
 
-}
-function plus () {
+plus.addEventListener('click', () => {
+    if (numberOne == 0) {
+        numberOne = Number(calcArea.textContent);
+        operation = "plus";
+        emptyArea = "T";
+    }
+    else {
+        numberTwo = Number(calcArea.textContent);
+        let sum = numberOne + numberTwo;
+        numberOne = sum;
+        numberTwo = 0;
+        calcArea.textContent = sum;
+        operation = "";
+        emptyArea = "T";
+        
+    }
+})
 
-}
 
